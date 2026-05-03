c3e2c874357ce4713e31f6690c97fe84ae3b8950
RTnF <50510242+RTnF@users.noreply.github.com>
2026-05-04 08:58:18 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <p>AtCoder / ABC / ABC001 / A</p>
+          <h1>積雪深差</h1>
+          <p>Simple</p>
+          <p>
+            <a href="https://atcoder.jp/contests/abc001/tasks/abc001_1">問題</a>
+            {" / "}
+            <a href="https://atcoder.jp/contests/abc001/submissions/75474831">
+              提出
+            </a>
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
